4c1d2f8a8a3d804f0dfafa17461ba91c528f55b3
Henry Chipman <henry@tyemill.com>
2015-12-07 15:09:28 -0800

    info windows working

--- app/models/earthquake.rb
+++ app/models/earthquake.rb
@@ -2,8 +2,13 @@ class Earthquake < ActiveRecord::Base
 
 	def self.points
 		points = []
-		Earthquake.all.pluck(:intensity, :latitude, :longitude).each do |earthquake|
-			points.push({'intensity' => earthquake[0], 'latitude' => earthquake[1].to_s, 'longitude' => earthquake[2].to_s})
+		Earthquake.all.pluck(:intensity, :latitude, :longitude, :id).each do |earthquake|
+			points.push({
+				'id' => earthquake[3], 
+				'intensity' => earthquake[0], 
+				'latitude' => earthquake[1].to_s, 
+				'longitude' => earthquake[2].to_s
+			})
 		end
 		logger.debug(points)
 		return points
--- app/models/eruption.rb
+++ app/models/eruption.rb
@@ -2,8 +2,13 @@ class Eruption < ActiveRecord::Base
 
 	def self.points
 		points = []
-		Eruption.all.pluck(:vei, :latitude, :longitude).each do |eruption|
-			points.push({'vei' => eruption[0], 'latitude' => eruption[1].to_s, 'longitude' => eruption[2].to_s})
+		Eruption.all.pluck(:vei, :latitude, :longitude, :id).each do |eruption|
+			points.push({
+				'id' => eruption[3],
+				'vei' => eruption[0], 
+				'latitude' => eruption[1].to_s, 
+				'longitude' => eruption[2].to_s
+			})
 		end
 		return points
 	end
